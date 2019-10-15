57041340b99dec08f6ea7af9c6d018aea7f187fa
mrwalak <walakmatthew@gmail.com>
2019-10-14 22:02:13 -0400

    Created level import sequence (Levels stored in seperate .lua files)

---
+++ 1.lua
@@ -0,0 +1,8 @@
+local level_1 =  {
+    name = "Matt Walak",
+    data = {
+      face = "hello"
+    }
+}
+
+return level_1
---
+++ Levels/Tests/1.lua
@@ -0,0 +1,8 @@
+local level_1 =  {
+    name = "Test Level 1",
+    data = {
+      face = "hello"
+    }
+}
+
+return level_1
