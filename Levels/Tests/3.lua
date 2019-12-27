5d8afabf23ac88fb03221c178abec8d2f21fb766
Loaf! <walakmatthew@gmail.com>
2019-12-26 19:30:36 -0500

    Prepared to implement new clock synced transitions

--- Levels/Tests/3.lua
+++ Levels/Tests/3.lua
@@ -8,13 +8,17 @@ local speed = 10000
 
 -- Define pairs
 local obstacles_list = {}
-obstacles_list[1] = lb.pingpongPath_(speed)
+obstacles_list[1] = lb.obstacle(speed, 3)
+obstacles_list[12] = lb.obstacle(speed, 8)
+obstacles_list[23] = lb.obstacle(speed, 10)
+obstacles_list[34] = lb.obstacle(speed, 13)
 
 local level_3 =  {
     name = "Test level 3",
     speed = 4,
-    victory = 20,
+    victory = 100,
     obstacles = obstacles_list,
+    startScore = 0
 }
 
 return level_3
